ca5d0d00ee6fa02d7bfe9d8a64b541c72142e4cc
Juan Manuel Torres <juant@mindtouch.com>
2017-08-04 14:09:18 -0700

    Read from log stream and use a simple filter

--- LogParser/Function.cs
+++ LogParser/Function.cs
@@ -1,35 +1,42 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Amazon.CloudWatchLogs;
 using Amazon.Lambda.Core;
+using LogParser.Model;
+using Newtonsoft.Json;
 
 namespace LogParser {
-    
-    //--- Classes ---
-    public class CloudWatchLogsEvent {
-    
-        //--- Properties ---
-        public Awslogs awslogs { get; set; }
-    }
-    
-    public class Awslogs {
-        
-        //--- Properties ---
-        public string data { get; set; }
-    }
-
     public class Function {
     
         //--- Fields ---
         private readonly IAmazonCloudWatchLogs _cloudWatchClient;
-
-        //--- Methods ---
+        private const string FILTER = @"^(\[[A-Z ]+\])";
+        private static readonly Regex filter = new Regex(FILTER, RegexOptions.Compiled | RegexOptions.CultureInvariant); 
+        
+        //--- Constructors ---
         public Function() {
             _cloudWatchClient = new AmazonCloudWatchLogsClient();
         }
 
+        //--- Methods ---
         [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
         public void Handler(CloudWatchLogsEvent cloudWatchLogsEvent, ILambdaContext context) {
-            Console.WriteLine(cloudWatchLogsEvent.awslogs.data);
+            // Level One
+            Console.WriteLine($"THIS IS THE DATA: {cloudWatchLogsEvent.AwsLogs.Data}");
+            var data = DecompressLogData(cloudWatchLogsEvent.AwsLogs.Data);
+            Console.WriteLine($"THIS IS THE DECODED, UNCOMPRESSED DATA: {data}");
+            var events = JsonConvert.DeserializeObject<DecompressedEvents>(data).LogEvents;
+            var filteredEvents = events.Where(x => filter.IsMatch(x.Message)).ToList();
+            filteredEvents.ForEach(x => Console.WriteLine(x.Message));
+        }
+        
+        public static string DecompressLogData(string value) {
+            var gzip = Convert.FromBase64String(value);
+            using (GZipStream stream = new GZipStream(new MemoryStream(gzip), CompressionMode.Decompress)) 
+            return new StreamReader(stream).ReadToEnd();
         }
     }
 }
