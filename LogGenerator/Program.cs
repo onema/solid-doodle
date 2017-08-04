a53b344252acbe22137eb4b4572ab24dda81caff
Juan Manuel Torres <juant@mindtouch.com>
2017-08-03 19:32:14 -0700

    first commitgit status

---
+++ LogGenerator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Amazon;
+using Amazon.CloudWatchLogs;
+using Amazon.CloudWatchLogs.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+
+namespace LogGenerator {
+    class Program {
+        static void Main(string[] args) {
+            var region = RegionEndpoint.GetBySystemName("us-west-2");
+            var chain = new CredentialProfileStoreChain();
+            AWSCredentials awsCredentials;
+            if (chain.TryGetAWSCredentials("default", out awsCredentials)) { 
+                var client = new AmazonCloudWatchLogsClient(awsCredentials, region);
+                var request = new PutLogEventsRequest {
+                      LogGroupName = "juant/lambda/log-parser",
+                      LogStreamName = "Test1",
+                      LogEvents = new List<InputLogEvent> {
+                          new InputLogEvent {Message = "Foo", Timestamp = DateTime.Now}
+                          
+                      },
+                      SequenceToken = "49561000940692882142799072899892036996907997445775957554"
+                };
+                
+                var response = client.PutLogEventsAsync(request).Result;
+                Console.WriteLine(response.NextSequenceToken);
+            }
+        }
+    }
+}
---
+++ LogParser/Function.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Amazon.CloudWatchLogs;
+using Amazon.Lambda.Core;
+
+namespace LogParser {
+    
+    //--- Classes ---
+    public class CloudWatchLogsEvent {
+        public Awslogs awslogs { get; set; }
+    }
+    
+    public class Awslogs {
+        public string data { get; set; }
+    }
+
+    public class Function {
+    
+        //--- Fields ---
+        private readonly IAmazonCloudWatchLogs _cloudWatchClient;
+
+        //--- Methods ---
+        public Function() {
+            _cloudWatchClient = new AmazonCloudWatchLogsClient();
+        }
+
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public void Handler(CloudWatchLogsEvent cloudWatchLogsEvent, ILambdaContext context) {
+            Console.WriteLine(cloudWatchLogsEvent.awslogs.data);
+        }
+    }
+}
