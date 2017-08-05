86d8e0f0e125ddac8f0aaa19aa0da1f352b36522
Juan Manuel Torres <juant@mindtouch.com>
2017-08-04 19:17:15 -0700

    twitter streaming

--- LogGenerator/Program.cs
+++ LogGenerator/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Amazon;
 using Amazon.CloudWatchLogs;
 using Amazon.CloudWatchLogs.Model;
 using Amazon.Runtime;
 using Amazon.Runtime.CredentialManagement;
+using CoreTweet;
+using CoreTweet.Streaming;
 
 namespace LogGenerator {
     
@@ -22,69 +25,53 @@ namespace LogGenerator {
             AWSCredentials awsCredentials;
             if (chain.TryGetAWSCredentials("default", out awsCredentials)) { 
                 var client = new AmazonCloudWatchLogsClient(awsCredentials, region);
-                var request = new PutLogEventsRequest {
-                      LogGroupName = LOG_GROUP,
-                      LogStreamName = LOG_STREAM,
-                      LogEvents = new List<InputLogEvent> {
-                          new InputLogEvent {
-                              Message = "Test1\n", 
-                              Timestamp = DateTime.Now
-                          },
-                          new InputLogEvent {
-                              Message = "Error: foobar\n",
-                              Timestamp = DateTime.Now
-                          },
-                          new InputLogEvent {
-                              Message = "[ERROR] Foo\n",
-                              Timestamp = DateTime.Now
-                          },
-                          new InputLogEvent {
-                              Message = "Error: foobar\n",
-                              Timestamp = DateTime.Now
-                          },
-                          new InputLogEvent {
-                              Message = "[WARNING] Bar\n",
-                              Timestamp = DateTime.Now
-                          },
-                          new InputLogEvent {
-                              Message = "Error: foobar\n",
-                              Timestamp = DateTime.Now
-                          },
-                          new InputLogEvent {
-                              Message = "[Info] Should not show up\n",
-                              Timestamp = DateTime.Now
-                          },
-                          new InputLogEvent {
-                              Message = "[INFO] this should show up\n",
-                              Timestamp = DateTime.Now
-                          },
-                          new InputLogEvent {
-                              Message = "[BlA!]\n",
-                              Timestamp = DateTime.Now
-                          },
-                          new InputLogEvent {
-                              Message = "[BALH]\n",
-                              Timestamp = DateTime.Now
-                          },
-                          new InputLogEvent {
-                              Message = "[Soccer]\n",
-                              Timestamp = DateTime.Now
-                          },
-                          new InputLogEvent {
-                              Message = "[SOCCER] alalalalala goool\n",
-                              Timestamp = DateTime.Now
-                          },
-                          new InputLogEvent {
-                              Message = "ajk",
-                               Timestamp = DateTime.Now
-                          }
-                      },
-//                      SequenceToken = "49567750446880921213636912228400127364839351295405982594"
-                };
-                
-                var response = client.PutLogEventsAsync(request).Result;
-                Console.WriteLine(response.NextSequenceToken);
+                var sequenceToken = SequenceToken(client);
+                StartStreaming(client, sequenceToken);
             }
+        }
+
+        private static void StartStreaming(IAmazonCloudWatchLogs client, string sequenceToken) {
+            var counter = 0;
+            var logEvents = new List<InputLogEvent> ();
+            var tokens = GetTokens();
+            var messages = tokens.Streaming.StartStreamAsync(
+                StreamingType.Filter, 
+                new StreamingParameters(new List<KeyValuePair<string, object>> {new KeyValuePair<string, object>("track", "tea")})
+            ).Result;
+            foreach (var streamingMessage in messages) {
+                Console.WriteLine(streamingMessage.Json);
+                logEvents.Add(new InputLogEvent {
+                                          Message = streamingMessage.Json, 
+                                          Timestamp = DateTime.Now
+                                      });
+                if(counter % 10 == 0) {
+                    var request = new PutLogEventsRequest {
+                              LogGroupName = LOG_GROUP,
+                              LogStreamName = LOG_STREAM,
+                              LogEvents = logEvents,
+                              SequenceToken = sequenceToken
+                        };
+                        var response = client.PutLogEventsAsync(request).Result;
+                        sequenceToken = response.NextSequenceToken;
+                        logEvents = new List<InputLogEvent> ();
+                }
+            }
+        }
+
+        private static string SequenceToken(IAmazonCloudWatchLogs client) {
+            var response = client.DescribeLogStreamsAsync(new DescribeLogStreamsRequest {
+                LogGroupName = LOG_GROUP
+            }).Result;
+            
+            return response.LogStreams.First().UploadSequenceToken;
+        }
+
+        private static Tokens GetTokens() {
+            var consumerKey = "";
+            var consumerSecret = "";
+            var accessToken = "";
+            var accessSecret = "";
+            return Tokens.Create(consumerKey, consumerSecret, accessToken, accessSecret);
         }
     }
 }
