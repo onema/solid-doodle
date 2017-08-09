383e95e9cca8da51409099a4f49bdae496ff07ca
Juan Manuel Torres <juant@mindtouch.com>
2017-08-09 10:42:33 -0700

    save data to s3

--- LogParser/Function.cs
+++ LogParser/Function.cs
@@ -5,20 +5,27 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using Amazon.CloudWatchLogs;
 using Amazon.Lambda.Core;
+using Amazon.S3;
+using Amazon.S3.Model;
 using LogParser.Model;
 using Newtonsoft.Json;
 
 namespace LogParser {
     public class Function {
+        
+        //--- Constants ---
+        private const string FILTER = @"^(\[[A-Z ]+\])";
     
         //--- Fields ---
+        private readonly string logsBucket = Environment.GetEnvironmentVariable("LOGS_BUCKET");
         private readonly IAmazonCloudWatchLogs _cloudWatchClient;
-        private const string FILTER = @"^(\[[A-Z ]+\])";
+        private readonly IAmazonS3 _s3Client;
         private static readonly Regex filter = new Regex(FILTER, RegexOptions.Compiled | RegexOptions.CultureInvariant); 
         
         //--- Constructors ---
         public Function() {
             _cloudWatchClient = new AmazonCloudWatchLogsClient();
+            _s3Client = new AmazonS3Client();
         }
 
         //--- Methods ---
@@ -30,7 +37,14 @@ namespace LogParser {
             Console.WriteLine($"THIS IS THE DECODED, UNCOMPRESSED DATA: {data}");
             var events = JsonConvert.DeserializeObject<DecompressedEvents>(data).LogEvents;
             var filteredEvents = events.Where(x => filter.IsMatch(x.Message)).ToList();
-            filteredEvents.ForEach(x => Console.WriteLine(x.Message));
+            filteredEvents.ForEach(x => Console.WriteLine($"MESSAGE: {x.Message}"));
+            _s3Client.PutObjectAsync(
+                new PutObjectRequest(){
+                    BucketName = logsBucket,
+                    Key = Guid.NewGuid().ToString(),
+                    ContentBody = string.Join("\n", filteredEvents.Select(x => x.Message).ToArray())
+                }
+            ).Wait();
         }
         
         public static string DecompressLogData(string value) {
