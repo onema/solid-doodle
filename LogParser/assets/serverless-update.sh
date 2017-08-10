7fb77793c0020db4971bcb75012508aa381b1d19
Juan Manuel Torres <juant@mindtouch.com>
2017-08-10 10:26:39 -0700

    Update assets

--- LogParser/assets/serverless-update.sh
+++ LogParser/assets/serverless-update.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-dotnet restore
-dotnet build
-dotnet lambda package --configuration release --framework netcoreapp1.0 --output-package bin/release/netcoreapp1.0/deploy-package.zip --verbose
+# TO EXECUTE:
+# ./serverless-update -v --stage ENVIRONMENT_NAME --region us-west-2
+
+dotnet restore ../LogParser.csproj
+dotnet build ../LogParser.csproj
+(cd ../ ; dotnet lambda package ../LogParser.csproj --configuration release --framework netcoreapp1.0 --output-package bin/release/netcoreapp1.0/deploy-package.zip --verbose)
 serverless  deploy function --function LogParser -v $@
