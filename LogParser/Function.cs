91c2c32332c4711e9c2486ab2ebd47a5c6c74bbf
Juan Manuel Torres <juant@mindtouch.com>
2017-08-10 19:09:51 -0700

    Update function to break the message using the line separator

--- LogParser/Function.cs
+++ LogParser/Function.cs
@@ -1,9 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Text.RegularExpressions;
-using Amazon.CloudWatchLogs;
 using Amazon.Lambda.Core;
 using Amazon.S3;
 using Amazon.S3.Model;
@@ -14,17 +14,15 @@ namespace LogParser {
     public class Function {
         
         //--- Constants ---
-        private const string FILTER = @"^(\[[A-Z ]+\])";
+        private const string FILTER = @"(\[[\w ]+\])";
     
         //--- Fields ---
         private readonly string logsBucket = Environment.GetEnvironmentVariable("LOGS_BUCKET");
-        private readonly IAmazonCloudWatchLogs _cloudWatchClient;
         private readonly IAmazonS3 _s3Client;
         private static readonly Regex filter = new Regex(FILTER, RegexOptions.Compiled | RegexOptions.CultureInvariant); 
         
         //--- Constructors ---
         public Function() {
-            _cloudWatchClient = new AmazonCloudWatchLogsClient();
             _s3Client = new AmazonS3Client();
         }
 
@@ -38,15 +36,20 @@ namespace LogParser {
             
             // Level 2: Frame and filter events
             var events = JsonConvert.DeserializeObject<DecompressedEvents>(data).LogEvents;
-            var filteredEvents = events.Where(x => filter.IsMatch(x.Message)).ToList();
-            filteredEvents.ForEach(x => Console.WriteLine($"MESSAGE: {x.Message}"));
+            List<string> framedEvents = events.Select(x => x.Message.Split('\u03BB').ToList())
+                .SelectMany(i => i)
+                .Distinct()
+                .ToList();
+            framedEvents.ForEach(x => Console.WriteLine($"FRAMED MESSAGE: {x}"));
+            var filteredEvents = framedEvents.Where(x => filter.IsMatch(x)).ToList();
+            filteredEvents.ForEach(x => Console.WriteLine($"MESSAGE: {x}"));
             
             // Level 3: Save data to S3
             _s3Client.PutObjectAsync(
                 new PutObjectRequest(){
                     BucketName = logsBucket,
                     Key = Guid.NewGuid().ToString(),
-                    ContentBody = string.Join("\n", filteredEvents.Select(x => x.Message).ToArray())
+                    ContentBody = string.Join("\n", filteredEvents.Select(x => x).ToArray())
                 }
             ).Wait();
             
