85fce4a0958c961adcc0cbf42285ac6a76101284
Juan Manuel Torres <juant@mindtouch.com>
2017-08-11 11:15:03 -0700

    Using a command line parser to run the program

--- LogGenerator/Program.cs
+++ LogGenerator/Program.cs
@@ -1,120 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using Amazon;
-using Amazon.CloudWatchLogs;
-using Amazon.CloudWatchLogs.Model;
-using Amazon.Runtime;
-using Amazon.Runtime.CredentialManagement;
-using Tweetinvi;
-using System.Linq;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using Stream = Tweetinvi.Stream;
-using TwitterCredentials = LogGenerator.Model.TwitterCredentials;
+using Microsoft.Extensions.CommandLineUtils;
 
 namespace LogGenerator {
 
-    //--- Classes ---
-    public static class JsonExtensions {
-        public static bool IsNullOrEmpty(this JToken token) {
-            return (token == null) ||
-                   (token.Type == JTokenType.Array && !token.HasValues) ||
-                   (token.Type == JTokenType.Object && !token.HasValues) ||
-                   (token.Type == JTokenType.String && token.ToString() == String.Empty) ||
-                   (token.Type == JTokenType.Null);
-        }
-    }
-    
     class Program {
-
-        //--- Constants ---
-        const string AWS_REGION = "us-west-2";
-        const string LOG_GROUP = "/lambda-sharp/log-parser/dev";
-        const string LOG_STREAM = "test-log-stream";
 
         //--- Methods ---
         static void Main(string[] args) {
-            var region = RegionEndpoint.GetBySystemName(AWS_REGION);
-            var chain = new CredentialProfileStoreChain();
-            AWSCredentials awsCredentials;
-            if(!chain.TryGetAWSCredentials("lambdasharp", out awsCredentials)) {
-                throw new Exception("AWS Credentials not found!");
-            }
-            var client = new AmazonCloudWatchLogsClient(awsCredentials, region);
-            var sequenceToken = GetNextSequenceToken(client);
-            SetTwitterCredentials();
-            try {
-                StartStreaming(client, sequenceToken);
-            } catch(Exception e) {
-                Console.WriteLine(e);
-            }
-        }
-
-        private static void StartStreaming(IAmazonCloudWatchLogs client, string sequenceToken) {
-            var counter = 0;
-            var logEventsBatch = new List<InputLogEvent>();
-            var stream = Stream.CreateFilteredStream();
-            stream.AddTrack("cats");
-            // stream.AddLocation(new Coordinates(-74, 40), new Coordinates(-73, 41));
-            stream.MatchingTweetReceived += (sender, eventArgs) => {
-                var json = eventArgs.Tweet.ToJson();
-                var tweetInfo = JsonConvert.DeserializeObject<JObject>(json);
-                logEventsBatch.Add(new InputLogEvent {
-                    Message = GetLogText(tweetInfo),
-                    Timestamp = DateTime.Now
+            var app = new CommandLineApplication();
+            var generate = app.Command("generate", config => { 
+                config.OnExecute(()=>{
+                    config.ShowHelp(); //show help for generate
+                    return 1; //return error since we didn't do anything
                 });
-                if(++counter % 10 == 0) {
-                    sequenceToken = DispatchLogEvents(client, logEventsBatch, sequenceToken);
-                    logEventsBatch = new List<InputLogEvent>();
-                }
-            };
-            stream.StartStreamMatchingAllConditions();
-        }
-
-        private static string GetLogText(JObject tweetInfo) {
-            var user = tweetInfo["user"];
-            var retweetedStatus = tweetInfo["retweeted_status"];
-            var entities = tweetInfo["entities"];
-            var hashTags = entities["hashtags"];
-            var text = $"The user name is '{user["name"]}', they have {user["favourites_count"]} favorite tweets and have tweeted {user["statuses_count"]} times. They have {user["friends_count"]} friends and follow {user["followers_count"]} people!!\u03BB";
-            if (!retweetedStatus.IsNullOrEmpty()) {
-                var retweetCount = retweetedStatus["retweet_count"];
-                var favoriteCount = retweetedStatus["favorite_count"];
-                text += $"This tweet has been retweeted {retweetCount} times, and have been favorited by {favoriteCount} people\u03BB";
-            }
-            if (!hashTags.IsNullOrEmpty()) {
-                var hashTagsString = String.Join(", ", hashTags.Select(x => $"{x["text"]}"));
-                text += $"This tweet has the following hash tags: [{hashTagsString}]\u03BB";
-            }
-            text += $"The tweet message is: [{tweetInfo["text"]}]\u03BB";
-            return text;
-        }
-
-        private static void SetTwitterCredentials() {
-
-            // Set up twitter credentials (https://apps.twitter.com)
-            var credentials = JsonConvert.DeserializeObject<TwitterCredentials>(File.ReadAllText("credentials.json"));    
-            Auth.SetUserCredentials(credentials.ConsumerKey, credentials.ConsumerSecret, credentials.AccessToken, credentials.AccessSecret);
-        }
-        
-        private static string GetNextSequenceToken(IAmazonCloudWatchLogs client) {
-            var response = client.DescribeLogStreamsAsync(new DescribeLogStreamsRequest {
-                LogGroupName = LOG_GROUP
-            }).Result;
-            return response.LogStreams.First(x => x.LogStreamName == LOG_STREAM).UploadSequenceToken;
-        }
-
-        public static string DispatchLogEvents(IAmazonCloudWatchLogs client, List<InputLogEvent> logEvents, string sequenceToken) {
-            Console.WriteLine("Dispatching Log Events.");
-            var request = new PutLogEventsRequest {
-                  LogGroupName = LOG_GROUP,
-                  LogStreamName = LOG_STREAM,
-                  LogEvents = logEvents,
-                  SequenceToken = sequenceToken
-            };
-            var response = client.PutLogEventsAsync(request).Result;
-            return response.NextSequenceToken;
+                config.HelpOption("-? | -h | --help"); //show help on --help
+            });
+            generate.Command("help", config => { 
+                 config.Description = "get help!";
+                 config.OnExecute(()=>{
+                    generate.ShowHelp("generate");
+                     return 1;
+                 });
+             });
+            generate.Command("logs", config => {
+                config.Description = "generate logs using the twitter stream API";
+                config.HelpOption("-? | -h | --help");
+                var filterValue = config.Argument("filter", "value for the filter, e.g. cats, baseball, news", false);
+                config.OnExecute(()=>{ 
+                    TwitterStream.Run(-1, filterValue.Value);
+                    return 0;
+                });   
+            });
+            generate.Command("sample", config => {
+                config.Description = "generate a small sample of logs using the twitter stream API";
+                config.HelpOption("-? | -h | --help");
+                var number = config.Argument("number", "number of tweets to use to generate logs", false);
+                var filterValue = config.Argument("filter", "value for the filter, e.g. cats, baseball, news", false);
+                config.OnExecute(()=>{
+                    if (string.IsNullOrWhiteSpace(number.Value)) return 1;
+                    TwitterStream.Run(int.Parse(number.Value), filterValue.Value);
+                    return 0;
+                });   
+            });
+            app.HelpOption("-? | -h | --help");
+            var result = app.Execute(args);
+            Environment.Exit(result);
         }
     }
 }
