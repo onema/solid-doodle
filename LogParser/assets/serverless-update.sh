d3b3671c85be66000252e273bb34e06308c5e705
Juan Manuel Torres <kinojman@gmail.com>
2017-08-16 01:34:08 -0700

    Removing serverless-update as it was never used

--- LogParser/assets/serverless-update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# TO EXECUTE:
-# ./serverless-update -v --stage ENVIRONMENT_NAME --region us-west-2
-
-dotnet restore ../LogParser.csproj
-dotnet build ../LogParser.csproj
-(cd ../ ; dotnet lambda package ../LogParser.csproj --configuration release --framework netcoreapp1.0 --output-package bin/release/netcoreapp1.0/deploy-package.zip --verbose)
-serverless  deploy function --function LogParser -v $@
