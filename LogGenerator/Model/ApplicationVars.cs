a92b968d724867a53355c4b9030ff206bf932e8b
Juan Manuel Torres <kinojman@gmail.com>
2017-08-16 01:31:19 -0700

    Adding application vars to customize the log group and stream names

---
+++ LogGenerator/Model/ApplicationVars.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace LogGenerator.Model {
+    
+    //--- Classes ---
+    public class ApplicationVars {
+
+        //--- Properties ---
+        [JsonProperty("log_group")]
+        public string LogGroup { get; set; }
+    
+        [JsonProperty("log_stream")]
+        public string LogStream { get; set; }
+    }
+}
